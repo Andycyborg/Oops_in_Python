a3ebfb7b00e7844e44d9408dd7230468aa017728
Andycyborg <andyhaker606@gmail.com>
2026-02-19 23:45:10 +0530

    1st py code written

---
+++ 01_class_object.py
@@ -0,0 +1,14 @@
+# create a class with car brand and model
+
+class Car:  #class
+
+    def __init__(self,brand,model): #constructor
+        self.brand=brand
+        self.model=model
+
+    def get_car_info(self):  # method
+        print(f"car model is {self.model} and car brand is {self.brand}")
+
+my_car = Car("tata","safari")
+
+my_car.get_car_info()
