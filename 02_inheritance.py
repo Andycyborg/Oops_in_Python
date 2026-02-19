a3a6eb0bc98b66297c0507535569ca174e09b101
Andycyborg <andyhaker606@gmail.com>
2026-02-20 00:18:57 +0530

    inheritence code done

---
+++ 02_inheritance.py
@@ -0,0 +1,26 @@
+# create a class with car brand and model
+
+class Car:  #class
+
+    def __init__(self,brand,model): #constructor
+        self.brand=brand
+        self.model=model
+
+    def get_car_info(self):  # method
+        print(f"car model is {self.model} and car brand is {self.brand}")
+
+
+class ElectricCar(Car):
+    def __init__(self,brand,model,battery_size):
+         
+        super().__init__(brand,model) # super()--> To get constructor / method Property
+        self.battery_size=battery_size
+    
+    def get_car_info(self):
+        print(super().get_car_info(),f" Baterry Size is {self.battery_size}")
+
+my_car=Car("Tata","Safari") # class object 'Car'
+my_car.get_car_info()
+
+my_new_car=ElectricCar("Tesla","Model s","85kWh") #subclass object 'ElectricCar'
+my_new_car.get_car_info()
